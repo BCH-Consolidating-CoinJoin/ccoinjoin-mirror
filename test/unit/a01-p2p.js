c98afe4808e81f2e76895963d65fadbaf211086c
Chris Troutner <chris.troutner@gmail.com>
2018-12-04 08:20:40 -0800

    Added unit test for saveKnownPeers()

--- test/unit/a01-p2p.js
+++ test/unit/a01-p2p.js
@@ -5,6 +5,9 @@
 
 const assert = require('chai').assert
 
+const util = require('util')
+util.inspect.defaultOptions = { depth: 1 }
+
 // Mocking libraries
 const Network = require('./mocks/network-mock')
 
@@ -41,7 +44,41 @@ describe('p2p.js', () => {
       const data = p2p.openKnownPeers(filename)
       // console.log(`data: ${JSON.stringify(data, null, 2)}`)
 
-      assert.hasAllKeys(data, ['seenServers', 'verifiedServers', 'seenPeers', 'verifiedPeers'])
+      assert.hasAllKeys(data, [
+        'seenServers',
+        'verifiedServers',
+        'seenPeers',
+        'verifiedPeers'
+      ])
+    })
+  })
+
+  describe('saveKnownPeers', () => {
+    it('should save a change to file', async () => {
+      const filename = `${__dirname}/../../peers/known-peers.json`
+
+      // Read in the file.
+      const data = p2p.openKnownPeers(filename)
+      // console.log(`data: ${util.inspect(data)}`)
+
+      // Inject a piece of test data
+      data.seenServers.push('test-server')
+
+      // Save the file.
+      await p2p.saveKnownPeers(data)
+
+      // Re-open and read the file.
+      const data2 = p2p.openKnownPeers(filename)
+
+      // Assert that the data got saved to the file.
+      const evalData = data.seenServers.indexOf('test-server') > -1
+      assert.equal(evalData, true)
+
+      // Remove the data from the file
+      const cleanData = data2.seenServers.filter(x => x !== 'test-server')
+      data2.seenServers = cleanData
+      await p2p.saveKnownPeers(data2)
+      // console.log(`data2: ${util.inspect(data2)}`)
     })
   })
 })
