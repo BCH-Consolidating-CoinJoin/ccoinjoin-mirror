c617bdd44565418ef7faf3041a04ceb96a266c68
Chris Troutner <chris.troutner@gmail.com>
2018-12-06 04:34:35 -0800

    Successfully validated ccoinjoin server

--- src/modules/cc-server/controller.js
+++ src/modules/cc-server/controller.js
@@ -6,43 +6,50 @@
 const rp = require('request-promise')
 
 async function addServer (ctx) {
-  const serverData = ctx.request.body.serverData
-  console.log(`serverData: ${JSON.stringify(serverData, null, 2)}`)
+  try {
+    const serverData = ctx.request.body.serverData
+    console.log(`serverData: ${JSON.stringify(serverData, null, 2)}`)
 
-  // Validation checks.
-  if (!serverData.serverType || serverData.serverType === '') {
-    ctx.throw(400, 'serverType can not be empty.')
-  }
+    // Validation checks.
+    if (!serverData) ctx.throw(400, `serverData object expected`)
 
-  if (!serverData.fee || serverData.fee === '') {
-    ctx.throw(400, 'fee can not be empty')
-  }
+    if (!serverData.serverType || serverData.serverType === '') {
+      ctx.throw(400, 'serverType can not be empty.')
+    }
 
-  if (!serverData.url || serverData.url === '') {
-    ctx.throw(400, 'url can not be empty')
-  }
+    if (!serverData.fee || serverData.fee === '') {
+      ctx.throw(400, 'fee can not be empty')
+    }
 
-  // Make an API call to ensure the server really exists.
-  const options = {
-    method: 'GET',
-    uri: `${serverData.url}/coinjoinout`,
-    // resolveWithFullResponse: true,
-    json: true
-  }
+    if (!serverData.url || serverData.url === '') {
+      ctx.throw(400, 'url can not be empty')
+    }
 
-  let result = await rp(options)
-  console.log(`result: ${JSON.stringify(result, null, 2)}`)
+    // Make an API call to ensure the server really exists.
+    const options = {
+      method: 'GET',
+      uri: `${serverData.url}/coinjoinout`,
+      // resolveWithFullResponse: true,
+      json: true
+    }
 
-  // Add the server to the known-peers information.
+    let result = await rp(options)
+    console.log(`result: ${JSON.stringify(result, null, 2)}`)
 
-  /*
+    // Add the server to the known-peers information.
+
+    /*
   ctx.body = {
     ipfsid: process.env.IPFS_ID,
     orbitAddr: process.env.ORBITDB_ID
   }
   */
 
-  ctx.body = true
+    ctx.body = true
+  } catch (err) {
+    console.log(`Error /ccservers: `, err)
+    ctx.throw(400, err)
+  }
 }
 
 module.exports = {
