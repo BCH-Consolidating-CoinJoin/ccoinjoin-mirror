afc0063376f945de70f26b64c3d6f945ada9f0c0
Chris Troutner <chris.troutner@gmail.com>
2018-12-06 03:53:55 -0800

    prototyping /ccservers handler

--- src/modules/cc-server/controller.js
+++ src/modules/cc-server/controller.js
@@ -1,14 +1,37 @@
 /*
-
+  This is an API handler for Consolidating CoinJoin servers to register
+  themselves with the Mirror.
 */
+
+const rp = require('request-promise')
 
 async function addServer (ctx) {
   const serverData = ctx.request.body.serverData
   console.log(`serverData: ${JSON.stringify(serverData, null, 2)}`)
 
   // Validation checks.
+  if (!serverData.serverType || serverData.serverType === '') {
+    ctx.throw(400, 'serverType can not be empty.')
+  }
+
+  if (!serverData.fee || serverData.fee === '') {
+    ctx.throw(400, 'fee can not be empty')
+  }
+
+  if (!serverData.url || serverData.url === '') {
+    ctx.throw(400, 'url can not be empty')
+  }
 
   // Make an API call to ensure the server really exists.
+  const options = {
+    method: 'GET',
+    uri: `${serverData.url}/coinjoinout`,
+    // resolveWithFullResponse: true,
+    json: true
+  }
+
+  let result = await rp(options)
+  console.log(`result: ${JSON.stringify(result, null, 2)}`)
 
   // Add the server to the known-peers information.
 
